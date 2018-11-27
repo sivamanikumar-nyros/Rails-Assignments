f7e3e57542a9c96798773d39be0a61143a8b8f90
siva-nyros <siva.nyros@gmail.com>
2018-11-27 08:23:25 +0530

    assignment 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  
-	root:to => 'book#page_1'
-		get 'book/page_2'
-		get 'book/page_3'
-		get 'book/home'
-		get 'book/services'
-		get 'book/contact'
-  	
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  resources :articles
+ 
+  root 'articles#index'
+
 end
