c3d0d76ae0ac83d4411ac6532403242368845d55
siva-nyros <siva.nyros@gmail.com>
2018-12-12 18:00:37 +0530

    Rails Active Record Query Interface

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,18 +4,15 @@ class Article < ApplicationRecord
   length:{minimum: 5}
   validates :category, :presence => true
   validates :checkbox, :presence => true
+  
   has_many :checkouts
   has_many :users , through: :checkouts
   belongs_to :author
   belongs_to :category
   
   after_create :create_action
-    
     def create_action
-
-    	self.story = "Accepted"
-
-    	self.save!
+      self.story = "Accepted"
+      self.save!
     end
-    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ApplicationRecord
-	
 	has_one :address
-	
 	accepts_nested_attributes_for :address
-	
 	has_and_belongs_to_many :reviews, join_table: "users_reviews"
-	
 	has_many :checkouts
-	
 	has_many :articles , through: :checkouts 
 end
