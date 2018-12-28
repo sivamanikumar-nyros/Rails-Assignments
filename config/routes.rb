caefd7c0e4a2bf37f02d3c5ee7c9a90dcd791b32
siva-nyros <siva.nyros@gmail.com>
2018-12-28 12:51:27 +0530

    Action controllers HTTP authenticstions

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 	resources :articles
 	resources :users
 	resources :customers
+
 	
   get 'sessions/create'
   get 'sessions/destroy'
   get 'admin' => 'admin#index'
+  
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
